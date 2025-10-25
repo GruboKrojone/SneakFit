f64df116e2f2770016fe95703e184848ea36ee27
Kobbe <korbanj1108@gmail.com>
2025-10-25 22:11:53 +0200

    SF-48 Update dish

--- Backend/API/Controllers/DishController.cs
+++ Backend/API/Controllers/DishController.cs
@@ -11,6 +11,11 @@ public class DishController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
     [Route("add")]
-    public async Task<Unit> AddDish(AddDishParams @params, CancellationToken cancellationToken)
+    public async Task<Unit> AddDish(DishParams @params, CancellationToken cancellationToken)
         => await mediator.Send(new AddDishCommand(@params), cancellationToken);
+
+    [HttpPut]
+    [Route("{dishId}/update")]
+    public async Task<DishDto> UpdateDish(int dishId, DishParams @params, CancellationToken cancellationToken)
+        => await mediator.Send(new UpdateDishCommand(dishId, @params), cancellationToken);
 }
--- Backend/Domain/Dishes/Dto/DishParams.cs
+++ Backend/Domain/Dishes/Dto/DishParams.cs
@@ -1,11 +1,9 @@
 namespace Domain.Dishes.Dto;
 
-public record AddDishParams(
+public record DishParams(
     string Name,
     string? Description,
     int? Calories,
     int? Protein,
     int? Carbs,
-    int? Fat,
-    bool IsPublic
-    );
+    int? Fat);
