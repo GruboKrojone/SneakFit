b8ece84672d55d9425eaf52d50fdf1141d5e8d72
Kobbe <korbanj1108@gmail.com>
2026-01-14 07:09:05 +0100

    SF-118 Make class static

--- Backend/Algorithm/DishRecommendationAlgorithm.cs
+++ Backend/Algorithm/DishRecommendationAlgorithm.cs
@@ -2,14 +2,14 @@
 
 namespace Algorithm;
 
-public sealed class DishRecommendationAlgorithm
+public static class DishRecommendationAlgorithm
 {
     private const decimal RatesWeight = 0.4m;
     private const decimal PopularityWeight = 0.3m;
     private const decimal FavoriteMatchWeight = 0.2m;
     private const decimal CategoryDiversityWeight = 0.1m;
 
-    public IEnumerable<RecommendedDishResult> CalculateRecommendations(
+    public static IEnumerable<RecommendedDishResult> CalculateRecommendations(
         IEnumerable<DishData> allDishes,
         IEnumerable<int> userFavoriteDishIds,
         IEnumerable<int> userFavoriteCategoryIds,
