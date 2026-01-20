6b2f361244cbbea9b56e832326273087f04e0ad8
BacaKamil <kamil.baca@outlook.com>
2026-01-20 22:28:09 +0100

    SF-111 Fix conflicts

--- Frontend/src/pages/DishDetails.tsx
+++ Frontend/src/pages/DishDetails.tsx
@@ -13,7 +13,6 @@ import MacroCircle from "../components/MacroCircle";
 import DeleteDishModal from "../components/DeleteDishModal";
 import { toast } from "react-toastify";
 import ChatBubbleOutline from "@mui/icons-material/ChatBubbleOutline";
-import MacroCircle from "../components/MacroCircle";
 import CommentsModal from "../components/CommentsModal";
 import "./styles/DishDetails.css";
 import { useTranslation } from "react-i18next";
@@ -101,6 +100,8 @@ export default function DishDetails() {
       console.error("Error deleting dish:", error);
       toast.error(t("delete_dish_error"));
     }
+  };
+
   const handleCommentAdded = (comment: Comment) => {
     setComments((prevComments) => [...prevComments, comment]);
   };
