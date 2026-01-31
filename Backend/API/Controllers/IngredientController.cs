a1ec394c2fe1ad272456fd8470762376ae45ab93
Kobbe <korbanj1108@gmail.com>
2026-01-31 16:40:48 +0100

    SF-141 Add ingredient

---
+++ Backend/API/Controllers/IngredientController.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Dishes.Commands;
+using Domain.Dishes.Dto;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("ingredient")]
+public class IngredientController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [Route("add")]
+    public async Task<Unit> AddIngredient(IngredientParams @params, CancellationToken cancellationToken)
+        => await mediator.Send(new AddIngredientCommand(@params), cancellationToken);
+}
